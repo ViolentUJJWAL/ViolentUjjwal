90c8af89d10dfb663d8a79c4f47269fc09ac78eb
ViolentUJJWAL <ujjwal21aman08@gmail.com>
2024-12-28 14:04:26 +0530

    add deploy url

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
